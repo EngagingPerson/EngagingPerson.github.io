53825aed98a837b7cfe46155de2296e55c14a061
EngagingPerson <bsederho@gmail.com>
2024-09-01 20:22:13 -0500

    Update script.js

--- game/script.js
+++ game/script.js
@@ -1,1 +1,10 @@
+const textArea = document.querySelector('#writer-window'); // textarea or input
+textArea.addEventListener('input', calculateWords);
 
+function calculateWords(e) {
+  const splitArr = e.target.value.split(' ');
+  const newSplitArr = splitArr.filter((value) => {
+    return value; // returning values with content
+  });
+  console.log(newSplitArr);
+}
